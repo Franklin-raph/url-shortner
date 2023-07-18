c015253419ac5e3672d481c76bda1602540ba6b9
franklin-raph <igboekwulusifranklin@gmail.com>
2023-07-18 14:29:11 +0100

    updated the UI

--- main.js
+++ main.js
@@ -3,6 +3,29 @@ document.querySelector("button").addEventListener("click", ()=> {
     if(userInput.value.length === 0){
         return
     }else{
-        document.querySelector(".url-text-div").innerHTML = `<a href=${userInput.value} target="_blank">bit.ly/${userInput.value.substring(0,3)}</a>`
+        document.querySelector(".link-container .body").innerHTML += `
+                <div>
+                    <div class="url-text-div">
+                        <div>
+                            <p>the very very long link</p>
+                            <div>
+                                <p>short link</p>
+                                <div class="actions">
+                                    <a href=${userInput.value} target="_blank">
+                                      <i class="ri-external-link-fill"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        `
     }
 })
+
+
+{/* <i class="ri-clipboard-fill" onclick="copyLink(123)"></i>
+<i class="ri-share-fill"></i>
+function copyLink(generatedLink){
+  navigator.clipboard.writeText(generatedLink)
+} */}
